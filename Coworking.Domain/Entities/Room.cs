71c2cf27ced6f308b284bbb4b722011f28745b57
Samuel Loaiza <loaizaocampos@gmail.com>
2026-04-12 12:00:11 -0500

    feat: create of entities

---
+++ Coworking.Domain/Entities/Room.cs
@@ -0,0 +1,17 @@
+namespace Coworking.Domain.Entities;
+
+public class Room
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public int Capacity { get; private set; }
+    public decimal PricePerHour { get; private set; }
+
+    public Room(Guid id, string name, int capacity, decimal pricePerHour)
+    {
+        Id = id;
+        Name = name;
+        Capacity = capacity;
+        PricePerHour = pricePerHour;
+    }
+}
---
+++ Coworking.Domain/Entities/User.cs
@@ -0,0 +1,6 @@
+namespace Coworking.Domain.Entities;
+
+public class User
+{
+    
+}
